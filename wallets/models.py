77756e529195af7c14d6c28ee9df3cd9e73d4f7b
tim_rekelj <tim.rekelj@xlab.si>
2021-01-18 22:26:20 +0100

    Kinda working register... KINDA NOT

--- wallets/models.py
+++ wallets/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Wallet(models.Model):
+    title = models.CharField(max_length=100)
+    value = models.IntegerField(default=0)
+    date_created = models.DateField(default=timezone.now)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
