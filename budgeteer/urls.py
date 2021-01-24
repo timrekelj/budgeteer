320e26e7842d937bca613634ab976cb44e8525cb
tim_rekelj <tim.rekelj@xlab.si>
2021-01-25 00:52:17 +0100

    Made base.html for each app, added overview page, which is useless for now

--- budgeteer/urls.py
+++ budgeteer/urls.py
@@ -20,10 +20,8 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/', user_views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('profile/', user_views.profile, name='profile'),
+    path('', include('users.urls')),
     path('', include('home.urls')),
+    path('wallets', include('wallets.urls')),
 
 ]
--- wallets/views.py
+++ wallets/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
+
+def overview(request):
+    template = loader.get_template('wallets/overview.html')
+    context = {
+        'title' : 'Overview'
+    }
+    return HttpResponse(template.render(context, request))
