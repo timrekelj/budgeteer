3a62c1bcaa2daec8f5b31088fa5b7f31700c9b1d
tim_rekelj <tim.rekelj@xlab.si>
2021-01-27 00:50:26 +0100

    Added WalletListView but it is not working as there is no wallet objects and html is not designed to show them yet

--- wallets/urls.py
+++ wallets/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
+from .views import WalletListView
 from . import views
 
 urlpatterns = [
-    path('overview', views.overview, name='overview'),
+    path('overview', WalletListView.as_view(), name='overview'),
 ]
--- wallets/views.py
+++ wallets/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.views.generic import ListView
+from .models import Wallet
 
 # Create your views here.
 
@@ -10,3 +12,8 @@ def overview(request):
         'title' : 'Overview'
     }
     return HttpResponse(template.render(context, request))
+
+class WalletListView(ListView):
+    model = Wallet
+    template_name = 'wallets/overview.html' # <app>/<model>_<viewtype>.html
+    context_object_name = 'wallets'
